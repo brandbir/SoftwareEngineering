c166aae454956d75fb72f8fd20da4f091299355d
andrewcarlsammut <asam0032@um.edu.mt>
2014-04-08 17:56:43 +0200

    LastCommitFinal

--- src/main/java/com/pest/demo/Calculator.java
+++ src/main/java/com/pest/demo/Calculator.java
@@ -1,21 +1,30 @@
 package com.pest.demo;
 
 public class Calculator {
-	
-	public int add(int x, int y) {
-		return x+y;
+    
+	public int add(int a, int b)
+	{
+		int ans = a + b;
+		return ans;
 	}
 	
-	public int sub(int x, int y) {
-		return x-y;
+	public int subtract(int a, int b)
+	{
+		return a - b;
 	}
 	
-	public int multiply(int x, int y) {
-		return x*y;
+	public int multiply(int a, int b)
+	{
+		return a * b;
 	}
 	
-	public int divide(int x, int y) {
-		return x/y;
+	public int divide(int a, int b)
+	{
+		if(b == 0)
+			return -999;
+		else
+			return a/b;
 	}
-
+	
+    
 }
--- src/test/java/com/pest/demo/TestCalculator.java
+++ src/test/java/com/pest/demo/TestCalculator.java
@@ -6,28 +6,27 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class TestCalculator {
-
-	Calculator calc;
-	
-	@Before
-	public void setUp() throws Exception {
-		calc = new Calculator();
-	}
-
+public class CalculatorTest {
+    
 	@Test
-	public void testAddPositiveNumbers() {
-		assertEquals(5, calc.add(2, 3));
+	public void test1() {
+		Calculator calcTest = new Calculator();
+		assertEquals("2+2 = 4",4, calcTest.add(2, 2));//if it fails the message will be outputted in the failure trace
 	}
-	
 	@Test
-	public void testAddNegativeNumbers() {
-		assertEquals(-6, calc.add(-3, -3));
+	public void test2() {
+		Calculator calcTest = new Calculator();
+		assertEquals("2/0 = -999",-999, calcTest.divide(2, 0));
 	}
-	
 	@Test
-	public void testSubtraction() {
-		assertEquals(5, calc.divide(25, 5));
+	public void test3() {
+		Calculator calcTest = new Calculator();
+		assertEquals("2*3 = 6",6, calcTest.multiply(2, 3));
 	}
-
+	@Test
+	public void test4() {
+		Calculator calcTest = new Calculator();
+		assertEquals("3-1 = 2",2, calcTest.subtract(3, 1));
+	}
+    
 }
