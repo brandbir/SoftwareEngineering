8cd277833630a48e858e0c4743154df7bbcf19b3
andrewcarlsammut <asam0032@um.edu.mt>
2014-04-08 18:13:17 +0200

    LastFinalCommit2

--- src/test/java/com/pest/demo/TestCalculator.java
+++ src/test/java/com/pest/demo/TestCalculator.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class CalculatorTest {
+public class TestCalculator {
     
 	@Test
 	public void test1() {
