b9979ca2211da0002767e598036e4eacb3a9c3f1
andrewcarlsammut <asam0032@um.edu.mt>
2014-05-24 12:24:08 +0200

    mapfactory

---
+++ src/main/java/com/uom/factory/MapFactory.java
@@ -0,0 +1,34 @@
+package main.java.com.uom.factory;
+
+
+
+/**
+ * This is the map factory. This Map factory relies on its 2 subclasses (SafeMapCreator and HazardousMapCreator) to 
+ * define the factory method so that it returns an instance of the appropriate ConcreteProduct 
+ */
+public class MapFactory {    
+
+	public Map createMap(String type, int noOfPlayers, int mapSize)  //need type to choose which level of game to instantiate
+	{
+		MapFactory creator = findCreatorForType(type); 
+		return creator.createMap(noOfPlayers, mapSize);
+	}
+
+	public MapFactory findCreatorForType(String type)
+	{
+		if(type.equals("safe"))  
+		{
+			 return new SafeMapCreator(); 				//SafeMap Creator is a concrete product
+		}
+		else if(type.equals("hazardous"))
+		{
+			return new HazardousMapCreator();   //Hazardous Map is a concrete product
+		}
+		return null;
+	}
+	
+	public Map createMap(int noOfPlayers, int mapSize) {
+		return null;  
+	}
+
+}
